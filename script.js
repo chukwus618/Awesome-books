255ab79d11289a6a0d7d785829e6f11bc5026fdd
chukwus618 <chukwus618@gmail.com>
2025-11-12 17:45:56 +0100

    Use event delegation and array-filter to remove index-specific books, update local storage and render

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const books = JSON.parse(localStorage.getItem('books')) || [
+let books = JSON.parse(localStorage.getItem('books')) || [
   {
     title: 'Lorem ipsum',
     author: 'Testeroo Testyy',
@@ -11,11 +11,11 @@ const books = JSON.parse(localStorage.getItem('books')) || [
 ];
 
 const booksContainer = document.getElementById('books-container');
-books.forEach((book) => {
+books.forEach((book, index) => {
   booksContainer.innerHTML += `<section>
       <p>${book.title}</p>
       <p>${book.author}</p>
-      <button type="button">Remove</button>
+      <button type="button" class="remove-button" data-index="${index}">Remove</button>
       <hr>
   </section>`;
 });
@@ -30,14 +30,32 @@ addBtn.addEventListener('click', () => {
   };
   books.push(newBook);
   booksContainer.innerHTML = '';
-  books.forEach((book) => {
+  books.forEach((book, index) => {
     booksContainer.innerHTML += `<section>
       <p>${book.title}</p>
       <p>${book.author}</p>
-      <button type="button">Remove</button>
+      <button type="button" class="remove-button" data-index="${index}">Remove</button>
       <hr>
     </section>`;
   });
   localStorage.setItem('books', JSON.stringify(books));
   JSON.parse(localStorage.getItem('books'));
+});
+
+booksContainer.addEventListener('click', (e) => {
+  if (e.target.classList.contains('remove-button')) {
+    const index = Number(e.target.dataset.index);
+    books = books.filter((_, i) => i !== index);
+    localStorage.setItem('books', JSON.stringify(books));
+
+    booksContainer.innerHTML = '';
+    books.forEach((book, index) => {
+      booksContainer.innerHTML += `<section>
+        <p>${book.title}</p>
+        <p>${book.author}</p>
+        <button type="button" class="remove-button" data-index="${index}">Remove</button>
+        <hr>
+      </section>`;
+    });
+  }
 });
