80e25f5a6a294dcdbe16eff5041a067423dc1c47
chukwus618 <chukwus618@gmail.com>
2025-11-12 16:09:15 +0100

    Add event listener on 'add button' to automatically add new books from local storage

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const books = [
+const books = JSON.parse(localStorage.getItem('books')) || [
   {
     title: 'Lorem ipsum',
     author: 'Testeroo Testyy',
@@ -18,4 +18,26 @@ books.forEach((book) => {
       <button type="button">Remove</button>
       <hr>
   </section>`;
+});
+
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const addBtn = document.querySelector('#add-btn');
+addBtn.addEventListener('click', () => {
+  const newBook = {
+    title: title.value,
+    author: author.value,
+  };
+  books.push(newBook);
+  booksContainer.innerHTML = '';
+  books.forEach((book) => {
+    booksContainer.innerHTML += `<section>
+      <p>${book.title}</p>
+      <p>${book.author}</p>
+      <button type="button">Remove</button>
+      <hr>
+    </section>`;
+  });
+  localStorage.setItem('books', JSON.stringify(books));
+  JSON.parse(localStorage.getItem('books'));
 });
